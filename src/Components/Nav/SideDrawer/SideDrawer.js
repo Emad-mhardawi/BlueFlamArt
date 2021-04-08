2b98e115ad8a2f8325828e3dbefc4945a91ae5ce
Emad Mhardawi <emad.mhardawi@gmail.com>
2021-04-08 10:04:17 +0200

    added Sidebar with animation for small screens

--- src/Components/Nav/SideDrawer/SideDrawer.js
+++ src/Components/Nav/SideDrawer/SideDrawer.js
@@ -1,13 +1,33 @@
-import './SideDrawer.css';
+import {AnimatePresence, motion, usePresence} from'framer-motion';
+import {sideDrawerVariants, linksVariants } from '../../../framerAnimation/sideDrwaer';
+import { useEffect } from 'react';
+import { NavLink } from "react-router-dom";
+import "./SideDrawer.css";
 
+const SideDrawer = (props) => {
 
-const SideDrawer = ()=>{
-    return(
-        <div className='sideDrawer'>
-          
-        </div>
-    )
-}
+  return (
+    
+    <motion.ul  className="sideDrawer" exit='exit' variants={sideDrawerVariants} initial='hiddin' animate='visible'  >
+        <motion.li variants={linksVariants} className="sideDrawer-list-item">
+            <NavLink to="/" exact className="sideDrawer-list-link" onClick={props.closeMenu}  > Home</NavLink>
+        </motion.li>
+        
+        <motion.li variants={linksVariants} className="sideDrawer-list-item">
+            <NavLink to="/about" className="sideDrawer-list-link" onClick={props.closeMenu} >About </NavLink>
+        </motion.li>
 
+        <motion.li variants={linksVariants} className="sideDrawer-list-item">
+            <NavLink to="/contact" className="sideDrawer-list-link" onClick={props.closeMenu} >Contact </NavLink>
+        </motion.li>
+
+        <motion.li variants={linksVariants} className="sideDrawer-list-item">
+            <NavLink to="/work" className="sideDrawer-list-link" onClick={props.closeMenu} > Work</NavLink>
+        </motion.li>
+
+    </motion.ul>
+  
+  );
+};
 
 export default SideDrawer;
